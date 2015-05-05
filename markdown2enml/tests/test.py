e5cc1ef372e19b6f0b7f51819db97f90182b9c1e
Jared Crapo <jared@kotfu.net>
2015-05-05 11:17:45 -0700

    Added docstring to ENMLValidationTest

--- markdown2enml/tests/test.py
+++ markdown2enml/tests/test.py
@@ -6,6 +6,13 @@ import markdown2enml
 from lxml import etree
 
 class ENMLValidationTest(unittest.TestCase):
+    """Validate the xml output against the evernote DTD
+    
+    To avoid network access during testing, the evernote dtd and the
+    entities is references have been stored locally. To make this work,
+    I had to edit the enml2.dtd to reference the local entity files instead
+    of referencing the ones on http://www.w3.org
+    """
 
     def setUp(self):
         self.dtd = etree.DTD(open('enml2.dtd'))
